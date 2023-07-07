ffd4258fc2fc2fb68cb2bd1aa6653c5b60d33371
Charpentier07 <tristan07.charpentier@gmail.com>
2023-07-07 15:26:36 +0200

    Init Project

---
+++ public_html/Model/Challenge.php
@@ -0,0 +1,22 @@
+<?php
+
+class User {
+
+    private int $id ; 
+    
+    private string $firstname ; 
+
+    private string $lastName ; 
+
+    private int $pts; 
+
+    private string $image ;
+
+    
+
+
+
+
+}
+
+?>
---
+++ public_html/Model/User.php
@@ -0,0 +1,24 @@
+<?php
+
+class User {
+
+    private int $id ; 
+    
+    private int $user_id ; 
+
+    private string $name ; 
+
+    private int $pts; 
+
+    private bool $fait ;
+
+    private string $image ; 
+
+    
+
+
+
+
+}
+
+?>
